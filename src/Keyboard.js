49e64ee44751176b605afb2ac1606ff5bb42e29f
Sergio Campos <scarfax@gmail.com>
2022-10-14 13:51:49 -0300

    Inserindo partes do HTML e CSS

---
+++ src/Keyboard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Keyboard = () => {
+  return (
+    <section className="keyboard">
+      <button className="key key_pom">Pom</button>
+      <button className="key key_clap">Clap</button>
+      <button className="key key_tim">Tim</button>
+
+      <button className="key key_puff">Puff</button>
+      <button className="key key_splash">Splash</button>
+      <button className="key key_toim">Toim</button>
+
+      <button className="key key_psh">Psh</button>
+      <button className="key key_tic">Tic</button>
+      <button className="key key_tom">Tom</button>
+    </section>
+  );
+};
+
+export default Keyboard;
