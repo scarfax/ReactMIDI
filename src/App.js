d730377cb5b8f38a902c7aa02ddc67680bab70f0
Sergio Campos <scarfax@gmail.com>
2022-10-27 16:10:16 -0300

    versão funcional 0.7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React from 'react';
 import './css/index.css';
-import Keyboard from './Keyboard';
 
 function App() {
+  const start = (sound) => {
+    console.log(sound.target.innerText);
+    const audio = new Audio(`../../sounds/${sound.target.innerText}.wav`);
+    audio.play();
+  };
+
   return (
-    <div>
-      <h1>React Midi</h1>
-      <Keyboard />
+    <div className="keyboard">
+      <button className="key" onClick={start}>
+        Pom
+      </button>
+      <button className="key" onClick={start}>
+        Clap
+      </button>
+      <button className="key" onClick={start}>
+        Tim
+      </button>
+
+      <button className="key" onClick={start}>
+        Puff
+      </button>
+      <button className="key" onClick={start}>
+        Splash
+      </button>
+      <button className="key" onClick={start}>
+        Toim
+      </button>
+
+      <button className="key" onClick={start}>
+        Psh
+      </button>
+      <button className="key" onClick={start}>
+        Tic
+      </button>
+      <button className="key" onClick={start}>
+        Tom
+      </button>
     </div>
   );
 }
